f36914d6255fb2cede04e10f2f1bc76b107c83af
Ravidu Theekshana <125554585+0TiE@users.noreply.github.com>
2024-07-25 16:24:47 +0530

    SideNavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
+import SideNavBar from "./components/SideNavBar/SideNavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row  flex-fill">
+    <div className="col-1 ">
+   
+    <SideNavBar />
+
+      
     </div>
+    <div className=" col-11 pe-5 ">
+    <Header />
+     
+    </div>
+  </div>
   );
 }
 
